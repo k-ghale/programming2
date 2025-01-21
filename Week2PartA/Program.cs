c9e1eca74796f35a6bb4d9a89a1b963465b28e5f
k-ghale <kabinghale888@gmail.com>
2025-01-21 13:55:25 -0500

    Programming - 2

---
+++ Week2PartA/Program.cs
@@ -0,0 +1,125 @@
+﻿
+class Week2{
+
+  public static void Main(string[] args){
+    Medal m1 = new Medal("Horace Gwynne", "Boxing", MedalColor.Gold, 2012, true);
+    //print the object
+    Console.WriteLine(m1);
+    //print only the name of the medal holder
+    Console.WriteLine(m1.Name);
+    
+    //create another object
+    Medal m2 = new Medal("Michael Phelps", "Swimming", MedalColor.Gold, 2012, false);
+    //print the updated m2
+    Console.WriteLine(m2);
+
+    List<Medal> medals = new List<Medal>(){ m1, m2};
+    medals.Add(new Medal("Ryan Cochrane", "Swimming", MedalColor.Silver, 2012, false));
+    medals.Add(new Medal("Adam van Koeverden", "Canoeing", MedalColor.Silver, 2012, false));
+    medals.Add(new Medal("Rosie MacLennan", "Gymnastics", MedalColor.Gold, 2012, false));
+    medals.Add(new Medal("Christine Girard", "Weightlifting", MedalColor.Bronze, 2012, false));
+    medals.Add(new Medal("Charles Hamelin", "Short Track", MedalColor.Gold, 2014, true));
+    medals.Add(new Medal("Alexandre Bilodeau", "Freestyle skiing", MedalColor.Gold, 2012, true));
+    medals.Add(new Medal("Jennifer Jones", "Curling", MedalColor.Gold, 2014, false));
+    medals.Add(new Medal("Charle Cournoyer", "Short Track", MedalColor.Bronze, 2014, false));
+    medals.Add(new Medal("Mark McMorris", "Snowboarding", MedalColor.Bronze, 2014, false));
+    medals.Add(new Medal("Sidney Crosby ", "Ice Hockey", MedalColor.Gold, 2014, false));
+    medals.Add(new Medal("Brad Jacobs", "Curling", MedalColor.Gold, 2014, false));
+    medals.Add(new Medal("Ryan Fry", "Curling", MedalColor.Gold, 2014, false));
+    medals.Add(new Medal("Antoine Valois-Fortier", "Judo", MedalColor.Bronze, 2012, false));
+    medals.Add(new Medal("Brent Hayden", "Swimming", MedalColor.Bronze, 2012, false));
+
+    //prints a numbered list of 16 medals.
+    Console.WriteLine("\n\nAll 16 medals");
+    for(int i = 0; i <= medals.Count-1 ; i++){
+      Console.WriteLine($"{i+1}. {medals[i]}");
+    }
+
+    //prints a numbered list of 16 names (ONLY)
+    Console.WriteLine("\n\nAll 16 names");
+    for(int i = 0; i <= medals.Count-1 ; i++){
+      Console.WriteLine($"{i+1}. {medals[i].Name}");
+    }
+
+    //prints a numbered list of 9 gold medals
+    Console.WriteLine("\n\nAll 9 gold medals");
+    for(int i = 0,j = 0; i <= medals.Count-1 ; i++){
+      if(medals[i].Color == MedalColor.Gold){
+        Console.WriteLine($"{j+1}. {medals[i]}");
+        j++;
+      }
+    }
+
+    //prints a numbered list of 9 medals in 2012
+    Console.WriteLine("\n\nAll 9 medals");
+    for(int i = 0,j = 0; i <= medals.Count-1 ; i++){
+      if(medals[i].Year == 2012){
+        Console.WriteLine($"{j+1}. {medals[i]}");
+        j++;
+      }
+    }
+
+    //prints a numbered list of 4 gold medals in 2012
+    Console.WriteLine("\n\nAll 4 gold medals");
+    for(int i = 0,j = 0; i <= medals.Count-1 ; i++){
+      if(medals[i].Year == 2012 && medals[i].Color == MedalColor.Gold){
+        Console.WriteLine($"{j+1}. {medals[i]}");
+        j++;
+      }
+    }
+
+    //prints a numbered list of 3 world record medals
+    Console.WriteLine("\n\nAll 3 records");
+    for(int i = 0,j = 0; i <= medals.Count-1 ; i++){
+      if(medals[i].IsRecord == true){
+        Console.WriteLine($"{j+1}. {medals[i]}");
+        j++;
+      }
+    }
+
+    //saving all the medal to file Medals.txt
+    Console.WriteLine("\n\nSaving to file");
+    string fileName = "/home/neo/Code/Programming2/Medals.txt";
+    using(StreamWriter sw = new StreamWriter(fileName)){
+      foreach(var medal in medals){
+        sw.WriteLine(medal);
+      }
+    }
+    Console.WriteLine($"Saved to the file {fileName}");
+
+
+    }
+
+ }
+
+enum MedalColor{
+  Bronze,
+  Silver,
+  Gold
+}
+
+class Medal{
+  public string Name {get;}
+  public string TheEvent {get;}
+  public MedalColor Color{get;}
+  public int Year{get;}
+  public bool IsRecord{get;}
+
+  public Medal(string name, string theEvent, MedalColor color, int year, bool isRecord){
+    Name = name;
+    TheEvent = theEvent;
+    Color = color;
+    Year = year;
+    IsRecord = isRecord;
+  }
+
+    public override string ToString()
+      {
+          return $"{Year} - {TheEvent}{(IsRecord == true ? "(R)" : "")}  {Name}({Color})";
+      }
+
+
+}
+
+
+
---
+++ Week2PartB/Program.cs
@@ -0,0 +1,75 @@
+﻿
+class Week2Part2{
+  public static void Main(string[] args){
+      //obj1
+      Pet pet1 = new Pet("Alex",2,"Dog");
+
+      //obj2
+      Pet pet2 = new Pet("Bonny",10,"Cat");
+      pet2.SetOwner("Ghale");
+      pet2.Train();
+
+      //obj3
+      Pet pet3 = new Pet("Max",7,"Bird");
+
+      //obj4
+      Pet pet4 = new Pet("Chester",5,"Goat");
+      pet4.SetOwner("Leo");
+      pet4.Train();
+
+      //Creating a list to store the above objects
+      List<Pet> p1 = new List<Pet>{pet1, pet2, pet3, pet4};
+      
+      //Using Methods 
+      p1[0].Train();
+      p1[0].SetOwner("Kabin");
+
+      //Loop to display the list
+      for(int i = 0; i <= p1.Count - 1 ; i++){
+        Console.WriteLine(p1[i]);
+      }
+
+      //Prompt the user of his name and display only his pet
+      Console.WriteLine("\nPlease Enter Your Name : ");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("\nPets with the given owner name : ");
+      for(int i = 0 ; i <= p1.Count - 1 ; i++){
+        if(p1[i].Owner == name){
+          Console.WriteLine(p1[i]);
+        }
+      }
+  }
+}
+
+class Pet{
+  public string Name{get;} 
+  public string Owner{get; private set;}
+  public int Age{get;}
+  public string Description{get;}
+  public bool IsHouseTrained{get; private set;}
+
+  public Pet(string name, int age, string description){
+    Name = name;
+    Age = age;
+    Description = description;
+    Owner = "no one";
+    IsHouseTrained = false;
+  }
+
+  public override string ToString()
+    {
+       return $"\nName : {Name} \nAge : {Age} \nDescription : {Description} \nOwner : {Owner} \nIsHouseTrained : {IsHouseTrained}";
+    }
+
+  public void SetOwner(string owner){
+    Owner = owner;
+  }
+
+  public void Train(){
+     IsHouseTrained = true;
+  }
+
+}
+
+
