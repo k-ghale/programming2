c8baf3e640e9f916bb22ae843ba6f9215ec9dff4
k-ghale <kabinghale888@gmail.com>
2025-01-23 05:55:59 -0500

    List Bubble Sortls

--- Week3PartA/Program.cs
+++ Week3PartA/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Week3PartA
+{
+   public static void Main(string[] args){
+        List<int> l1 = new List<int>();
+
+        l1.Add(4);
+        l1.Add(2);
+        l1.Add(3);
+        l1.Add(1);
+
+        foreach (int item in l1)
+        {
+           Console.WriteLine(item); 
+        }
+
+        Console.WriteLine();
+
+        List<int> sorted = bubSort(l1);
+
+        foreach (int i in sorted)
+        {
+           Console.WriteLine(i); 
+        }
+
+   }
+
+   public  static List<int> bubSort(List<int> l1){
+       for (int i = 0; i < l1.Count - 1; i++)
+       {
+           for (int j = 0; j < l1.Count - i - 1; j++)
+           {
+               if(l1[j] > l1[j+1]){
+                   int temp = l1[j];
+                   l1[j] = l1[j+1];
+                   l1[j+1] = temp;
+               } 
+           }
+       }
+       return l1;
+   }
+}
+
